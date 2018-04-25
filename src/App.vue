a96f88abbb7a55c5e7d69a3216736f8da418d5ca
lauragift21 <laurandidi21@gmail.com>
2018-04-25 16:22:42 +0100

    Initial commit :fire:

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div class="banner">
+      <img src="https://cdn-images-1.medium.com/max/880/1*tidWIoAQ-i2SiKTNh9WJSw.png" style="margin: 14px; cursor: pointer;"  width="100px" alt="logo">
+    </div>
+    <VideoPlayer/>
+  </div>
+</template>
+
+<script>
+import VideoPlayer from './components/VideoPlayer'
+
+export default {
+  name: 'App',
+  components: {
+    VideoPlayer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
